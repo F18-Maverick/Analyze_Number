c61a73f1934fee5399a8b569cff6bab181c73c6b
F18-Maverick <xu_ruihong2009@163.com>
2025-09-10 20:50:11 +0800

    add the sign in button

--- Numers_Analyze_Beta/Analyze_number_mainfile/ticket_12306/get_ticket_interface.py
+++ Numers_Analyze_Beta/Analyze_number_mainfile/ticket_12306/get_ticket_interface.py
@@ -149,6 +149,15 @@ class Get_ticket_interface:
         self.button_update_info_place = self.button_update_info.place(x=self.x_4/2, y=self.y_4, anchor="center")
         self.update_info_control = self.button_update_info.bind(
             "<Button-1>", lambda event_1: self.update_station_info_thread())
+        self.button_sign_in = tkinter.Button(
+            self.Windows, text="登录", width=8, height=1, font=("Arial", 8, "underline"))
+        self.button_sign_in_width = self.button_sign_in.winfo_width()
+        self.button_sign_in_height = self.button_sign_in.winfo_height()
+        self.x_5 = int(self.Windows_width - self.button_sign_in_width)-30
+        self.y_5 = 200
+        self.button_sign_in_place = self.button_sign_in.place(x=self.x_5, y=self.y_5, anchor="center")
+        # self.update_info_control = self.button_update_info.bind(
+        #     "<Button-1>", lambda event_1: self.update_station_info_thread())
 
 
 
